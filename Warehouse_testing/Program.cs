a66c099206a56edaebf7ff8c8259f12b4cd8e50c
Newlot91 <erm.alexander.1991@gmail.com>
2021-09-01 14:20:31 +0300

    Day_11 ready;

--- Warehouse_testing/Program.cs
+++ Warehouse_testing/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
-namespace Warehouse_testing
+using WarehouseLibrary;
+namespace WarehouseApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Warehouse myWarehouse = new Warehouse();
+            myWarehouse.addInfo(myWarehouse);
+            Console.WriteLine();
+            Console.WriteLine("Information about your warehouse");
+            myWarehouse.displayInfo(myWarehouse);
+            Console.WriteLine();
+            Console.WriteLine("Enter the number of line that you want to change");
+            Console.WriteLine("1.Title, 2.Address, 3.Contact Number");
+            int numOfLine = int.Parse(Console.ReadLine());
+            myWarehouse.updateInfo(myWarehouse,numOfLine);
+            Console.WriteLine("Information about your warehouse");
+            myWarehouse.displayInfo(myWarehouse);
+            
+
         }
     }
 }
