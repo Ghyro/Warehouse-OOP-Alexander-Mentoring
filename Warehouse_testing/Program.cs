4e198aebfc7c85340bce838e935a26e51f61f3e5
Newlot91 <erm.alexander.1991@gmail.com>
2021-09-09 18:25:29 +0300

    Added TryParse;

--- Warehouse_testing/Program.cs
+++ Warehouse_testing/Program.cs
@@ -5,30 +5,24 @@ namespace WarehouseApp
 {
     class Program
     {
-        /*на доработку:
-        -добавить исключения для ввода консоли + добавить TryParse;
-        -дописать перегрузки конструкторов;
-
-
-        */
         static void Main(string[] args)
         {
-            Warehouse myWarehouse = new Warehouse("Store", "Belarus", "+375293628848") { Title = "Store", Address = "Belarus", ContactNumber = "+375293628848" };
+            Warehouse myWarehouse = new Warehouse("Store", "Belarus", "+375293628848", 5);
             WarehouseService warehouseService = new WarehouseService();
             Employee[] employees = new Employee[1];
-            int freeVacation = myWarehouse.Vacations - myWarehouse.NumOfEmployed;
             while (true)
             {
                 Console.WriteLine("\n1. Display info(title, address, contact number)");
                 Console.WriteLine("2. Update info(for instance, update contact number)");
                 Console.WriteLine("3. Clear all info about Warehouse(re - create Warehouse)");
-                Console.WriteLine("4. Display info about free vacancies");//не корректно работает, возможно не изменяет значение из freeVacation на 13 строке.
+                Console.WriteLine("4. Display info about free vacancies");
                 Console.WriteLine("5. Keep the list of Employees");
                 Console.WriteLine("6. Add employee");
                 Console.WriteLine("7. Search the employee by Name and Surname");
                 Console.WriteLine("8. Quit employee(remove)");
                 Console.WriteLine();
-                int operation = int.Parse(Console.ReadLine());
+                int operation;
+                int.TryParse(Console.ReadLine(), out operation);
 
                 switch (operation) //main menu
                 {
@@ -42,11 +36,12 @@ namespace WarehouseApp
                         {
                             Console.WriteLine("Enter the number of line that you want to change");
                             Console.WriteLine("1.Title, 2.Address, 3.Contact number, 4.Vacation");
-                            int numOfLine = int.Parse(Console.ReadLine());
+                            int numOfLine;
+                            int.TryParse(Console.ReadLine(), out numOfLine);
                             if (numOfLine == 4)
                             {
                                 Console.WriteLine($"Total vacation now: {myWarehouse.Vacations}");
-                                Console.WriteLine($"Total free vacation now: {freeVacation}");
+                                Console.WriteLine($"Total free vacation now: {myWarehouse.Vacations - myWarehouse.NumOfEmployed}");
                                 warehouseService.UpdateInfo(myWarehouse, numOfLine);
                                 break;
                             }
@@ -60,13 +55,13 @@ namespace WarehouseApp
                         }
                     case 4://Display info about free vacancies
                         {
-                            Console.WriteLine($"Free vacation now: {freeVacation}\n");
+                            Console.WriteLine($"Free vacation now: {myWarehouse.Vacations - myWarehouse.NumOfEmployed}\n");
                             break;
                         }
                     case 5://Keep the list of Employees
                         {
                             Console.WriteLine("List of employees:");
-                            KeepEmployeeList(employees);
+                            DisplayEmployeeList(employees);
                             break;
                         }
                     case 6://add employee
@@ -77,35 +72,37 @@ namespace WarehouseApp
                             Console.WriteLine("Enter your surname*");
                             string Surname = Console.ReadLine();
                             Console.WriteLine("Enter your age");
-                            int Age = int.Parse(Console.ReadLine());
+                            int Age;
+                            int.TryParse(Console.ReadLine(), out Age);
                             EnumVacation Job = 0;
                             Console.WriteLine("Select your job position:\n1.accountant\n2.cleaner\n3.director\n4.manager\n5.storekeeper");
-                            int selection = int.Parse(Console.ReadLine());
+                            int selection;
+                            int.TryParse(Console.ReadLine(), out selection);
                             switch (selection)
                             {
                                 case 1:
                                     {
-                                        Job = EnumVacation.accountant;
+                                        Job = EnumVacation.Accountant;
                                         break;
                                     }
                                 case 2:
                                     {
-                                        Job = EnumVacation.cleaner;
+                                        Job = EnumVacation.Cleaner;
                                         break;
                                     }
                                 case 3:
                                     {
-                                        Job = EnumVacation.director;
+                                        Job = EnumVacation.Director;
                                         break;
                                     }
                                 case 4:
                                     {
-                                        Job = EnumVacation.manager;
+                                        Job = EnumVacation.Manager;
                                         break;
                                     }
                                 case 5:
                                     {
-                                        Job = EnumVacation.storekeeper;
+                                        Job = EnumVacation.Storekeeper;
                                         break;
                                     }
                                 default:
@@ -118,7 +115,7 @@ namespace WarehouseApp
                             string ContactNumber = Console.ReadLine();
                             Console.WriteLine("Enter you education");
                             string Education = Console.ReadLine();
-                            employees[employees.Length - 1] = new Employee(Name, Surname, Age, Job, HomeAddress, ContactNumber, Education);//не добавляется объект в массив
+                            employees[employees.Length - 1] = new Employee(Name, Surname, Age, Job, HomeAddress, ContactNumber, Education);
                             myWarehouse.NumOfEmployed++; //я бы добавил в конструктор или еще куда-нибудь в класс, чтобы этот функционал выполнялся в из метода.
                             Array.Resize(ref employees, employees.Length + 1);
                             break;
@@ -129,13 +126,14 @@ namespace WarehouseApp
                             string searcingName = Console.ReadLine();
                             Console.WriteLine("Enter searcing surname");
                             string searchingSurname = Console.ReadLine();
-                            KeepEmployeeList(warehouseService.SearchEmployee(employees, searcingName, searchingSurname));
+                            DisplayEmployeeList(warehouseService.SearchEmployee(employees, searcingName, searchingSurname));
                             break;
                         }
                     case 8://Quit employee(remove)
                         {
                             Console.WriteLine("Enter number of employee from Employees list: ");
-                            int numOfEmployeeForQuit = int.Parse(Console.ReadLine());
+                            int numOfEmployeeForQuit;
+                            int.TryParse(Console.ReadLine(), out numOfEmployeeForQuit);
                             warehouseService.QuitEmployee(myWarehouse, employees, numOfEmployeeForQuit);
                             break;
                         }
@@ -144,7 +142,7 @@ namespace WarehouseApp
                         break;
                 }
 
-                void KeepEmployeeList(Employee[] employees) //перенёс в програм, т.к. получается ошибка из библиотеки. почему? Какие есть варианты для массива, чтобы его длина совпадала с кол-вом инициализированных объектов в нем
+                void DisplayEmployeeList(Employee[] employees)
                 {
                     if (myWarehouse.NumOfEmployed > 0)
                     {
@@ -175,7 +173,6 @@ namespace WarehouseApp
                     Console.WriteLine($"number of free vacation: {freeVacations}");
                     Console.WriteLine();
                 }
-
             }
         }
     }
