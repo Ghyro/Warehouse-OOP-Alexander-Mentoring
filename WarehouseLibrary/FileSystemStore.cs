24a0d417724fb1b75f46b95eced173857f950aad
Newlot91 <erm.alexander.1991@gmail.com>
2021-11-18 13:46:55 +0300

    Added FileSystemStore class;

---
+++ WarehouseLibrary/FileSystemStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseLibrary
+{
+    public static class FileSystemStore
+    {
+        public static void SaveData(string path, Warehouse warehouse)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, warehouse);
+            }
+
+        }
+        public static void LoadData(string path, ref Warehouse warehouse)//TODO добавить проверку на существования файла (try-catch)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {  
+                Warehouse deserilizeWarehouse = (Warehouse)formatter.Deserialize(fs);
+                warehouse = deserilizeWarehouse;                
+            }
+        }
+    }
+}
