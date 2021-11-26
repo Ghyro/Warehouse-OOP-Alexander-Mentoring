750510bb2ca934d300cbb735434fec013ba725a2
Newlot91 <erm.alexander.1991@gmail.com>
2021-11-26 09:10:17 +0300

    Added Logging feature;

---
+++ WarehouseLibrary/LogType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseLibrary
+{
+    public enum LogType
+    {
+        Warning=0,
+        Error,
+        Success
+    }
+}
